6e75f08b79dbf5c02fce6ab3238fd2a92d9ef9a6
Govind Raj Gupta <gg7078417058@gmail.com>
2025-01-01 22:07:37 +0530

    appwrite integrated

---
+++ lib/actions/patient.actions.ts
@@ -0,0 +1,30 @@
+'use server'
+
+import { ID,Query } from "node-appwrite"
+import { parseStringify } from "../utils";
+import { users } from "../appwrite.config";
+
+export const createUser = async (user: CreateUserParams) => {
+    try {
+        const newUser = await users.create(
+            ID.unique(), 
+            user.email, 
+            user.phone, 
+            undefined, 
+            user.name
+        ); 
+        console.log({newUser})
+
+        return parseStringify(newUser);
+
+    } catch (error: any) {
+        if (error && error?.code === 409) {
+            const existingUser = await users.list([
+                Query.equal('email', [user.email])
+            ])
+
+            return existingUser.users[0];
+        }
+    }
+}
+
